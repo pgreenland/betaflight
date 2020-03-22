3f35e6da05187e9bfb969618c12919b728a5c0fc
Phil Greenland <phil@quantulum.co.uk>
2020-03-22 19:11:24 +0000

    Add tag to version to identify my build.

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -22,7 +22,7 @@
 
 #include "common/utils.h"
 
-#define FC_FIRMWARE_NAME            "Betaflight"
+#define FC_FIRMWARE_NAME            "Betaflight_PG"
 #define FC_VERSION_MAJOR            4  // increment when a major release is made (big new feature, etc)
 #define FC_VERSION_MINOR            1  // increment when a minor release is made (small new feature, change etc)
 #define FC_VERSION_PATCH_LEVEL      5  // increment when a bug is fixed
